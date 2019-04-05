25454517aa4ab7169687dcc84d849894bfa1ee1e
George Lin <17georgel@gmail.com>
2019-04-05 21:21:27 +0800

    Message player radius for /near command

--- src/main/java/co/lotc/lever/cmd/Near.java
+++ src/main/java/co/lotc/lever/cmd/Near.java
@@ -1,18 +1,17 @@
 package co.lotc.lever.cmd;
 
-import static net.md_5.bungee.api.ChatColor.*;
-
-import java.util.stream.Collectors;
-
-import org.apache.commons.lang.StringUtils;
-import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
-
 import co.lotc.core.bukkit.util.ChatBuilder;
 import co.lotc.core.bukkit.util.LocationUtil;
 import co.lotc.core.command.annotate.Flag;
 import co.lotc.lever.BaseCommand;
 import lombok.var;
+import org.apache.commons.lang.StringUtils;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+import java.util.stream.Collectors;
+
+import static net.md_5.bungee.api.ChatColor.*;
 
 public class Near extends BaseCommand {
 	
@@ -26,7 +25,7 @@ public class Near extends BaseCommand {
 		.map(Player::getName)
 		.collect(Collectors.toList());
 		
-		new ChatBuilder("Players nearby: ").color(GOLD).append( nears.size()).color(WHITE).send(p);
+		new ChatBuilder("Players nearby: (" + radius + ")").color(GOLD).append( nears.size()).color(WHITE).send(p);
 		String formatted = GRAY + StringUtils.join(nears, WHITE+", "+GRAY);
 		p.sendMessage(formatted);
 	}
