b75f278e0db48acc9bc4f152c9ba0463ce197dba
George Lin <17georgel@gmail.com>
2019-04-20 03:07:11 +0800

    Saved previous gamemode when double tapping sneak

--- src/main/java/co/lotc/lever/listener/SneakToggleListener.java
+++ src/main/java/co/lotc/lever/listener/SneakToggleListener.java
@@ -10,9 +10,12 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.event.player.PlayerToggleSneakEvent;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static net.md_5.bungee.api.ChatColor.GREEN;
 
@@ -20,6 +23,7 @@ import static net.md_5.bungee.api.ChatColor.GREEN;
 public class SneakToggleListener implements Listener {
 	private final Lever plugin;
 	private final List<String> sneakAttempts = Lists.newArrayList();
+	private final Map<Player, GameMode> previousGamemode = new HashMap<>();
 	
 	@EventHandler(ignoreCancelled = true, priority= EventPriority.MONITOR)
 	public void onSneak(PlayerToggleSneakEvent e){
@@ -32,7 +36,12 @@ public class SneakToggleListener implements Listener {
 				} else { //Shift pressed down twice in short time
 					p.sendMessage(GREEN + "Toggled gamemode");
 					if(p.getGameMode() != GameMode.SPECTATOR) {
+						previousGamemode.put(p, p.getGameMode());
 						p.setGameMode(GameMode.SPECTATOR);
+					} else if (previousGamemode.containsKey(p)) {
+						p.setGameMode(previousGamemode.get(p));
+						p.setAllowFlight(true);
+						p.setFlying(true);
 					} else {
 						p.setGameMode(GameMode.SURVIVAL);
 						p.setAllowFlight(true);
@@ -41,5 +50,10 @@ public class SneakToggleListener implements Listener {
 				}
 			}
 		}
+	}
+
+	@EventHandler
+	public void onQuit(PlayerQuitEvent event) {
+		previousGamemode.remove(event.getPlayer());
 	}
 }
