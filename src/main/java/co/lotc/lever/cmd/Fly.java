518216ceb92932b35905e2924d2b93be7cb1a836
Textarea <17-Textarea@users.noreply.gitlab.lordofthecraft.net>
2019-05-22 21:04:28 -0400

    /fly list
    Getting a list of online players who are server-side flying
    
    Is this really all you have to do to make an argument for a command? I feel like I'm missing something...

--- src/main/java/co/lotc/lever/cmd/Fly.java
+++ src/main/java/co/lotc/lever/cmd/Fly.java
@@ -1,11 +1,16 @@
 package co.lotc.lever.cmd;
 
+import co.lotc.core.command.annotate.Cmd;
 import co.lotc.core.command.annotate.Default;
 import co.lotc.lever.BaseCommand;
+import org.apache.commons.lang.StringUtils;
+import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
+
+import java.util.stream.Collectors;
 
 import static net.md_5.bungee.api.ChatColor.*;
 
@@ -24,5 +29,16 @@ public class Fly extends BaseCommand {
 		if (s != target) {
 			s.sendMessage(AQUA + "Fly toggled " + (target.getAllowFlight() ? GREEN + "ON" : RED + "OFF") + AQUA + " for " + WHITE + target.getName());
 		}
+	}
+
+	@Cmd("List people in fly mode")
+	public void list() {
+		String names = Bukkit.getOnlinePlayers().stream()
+				.filter(Player::getAllowFlight)
+				.map(Player::getName)
+				.collect(Collectors.joining(", "));
+
+		if (StringUtils.isEmpty(names)) names = "none...";
+		msg(AQUA + "Players in fly mode: " + names);
 	}
 }
