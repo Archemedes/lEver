ef8dd8a06cb34bb8a042ee7b16a2816525bfbf5f
George Lin <17georgel@gmail.com>
2019-06-07 23:44:33 +0800

    Fix /emote not working beyond the first word

--- src/main/java/co/lotc/lever/cmd/Emote.java
+++ src/main/java/co/lotc/lever/cmd/Emote.java
@@ -4,6 +4,7 @@ import co.lotc.core.bukkit.util.ChatBuilder;
 import co.lotc.core.bukkit.util.LocationUtil;
 import co.lotc.core.command.annotate.Arg;
 import co.lotc.core.command.annotate.Flag;
+import co.lotc.core.command.annotate.Joined;
 import co.lotc.lever.BaseCommand;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
@@ -14,7 +15,7 @@ public class Emote extends BaseCommand {
 
     @Flag(name="r",description="Emote broadcast radius",type=double.class)
     @Flag(name="rp",description="Show RP name at the start of the emote")
-    public void invoke(Player source, @Arg("Emote Text") String emoteText) {
+    public void invoke(Player source, @Arg("Emote Text") @Joined String emoteText) {
         final double broadcastRadius = hasFlag("r") ? getFlag("r") : 64.0;
 
         ChatBuilder b = new ChatBuilder(hasFlag("rp") ? source.getDisplayName() + " " : "[!] ").color(YELLOW).hover("Sent by " + source.getDisplayName() + ".")
