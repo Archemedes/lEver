c92f453d16d70d6cbcbd94bb142465f2b5fc9a3f
501warhead <501warhead@gmail.com>
2019-08-11 22:28:25 +0200

    Prevent attributes from being applied by people without a high level of permission

--- src/main/java/co/lotc/lever/cmd/Item.java
+++ src/main/java/co/lotc/lever/cmd/Item.java
@@ -11,6 +11,7 @@ import co.lotc.lever.BaseCommand;
 public class Item extends BaseCommand {
 
 	public void invoke(Player p, ItemStack is, @Arg("amount") @Default("1") @Range(min=1, max=64) int amount) {
+		validate(getSender().hasPermission("lever.item.admin") || (is.getItemMeta() == null || is.getItemMeta().getAttributeModifiers().size() < 1), "Cannot create an item that has attributes");
 		is.setAmount(amount);
 		p.getInventory().addItem(is);
 	}
